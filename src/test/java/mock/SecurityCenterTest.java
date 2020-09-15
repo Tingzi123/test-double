bca34b82e5d35b36984a043ba0e7218bbb3dd86e
Tingzi123 <1044373556@qq.com>
2020-09-15 18:27:49 +0800

    [chenting]:use mockito to fake door close

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -1,17 +1,20 @@
 package mock;
 
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.verify;
+import static org.mockito.internal.verification.VerificationModeFactory.times;
 
-
+@ExtendWith(MockitoExtension.class)
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
-    private static class MockDoorPanel extends DoorPanel{
+    /*private static class MockDoorPanel extends DoorPanel{
         private  Boolean closeBeCalled=false;
 
         @Override
@@ -23,19 +26,17 @@ public class SecurityCenterTest {
             return closeBeCalled;
         }
 
-    }
+    }*/
 
+    @Mock
+    private DoorPanel mockDoorPanel;
+
+    @InjectMocks
     private SecurityCenter securityCenter;
-    private MockDoorPanel mockDoorPanel;
-    @BeforeEach
-    public void setUp() {
-        mockDoorPanel=new MockDoorPanel();
-        securityCenter=new SecurityCenter(mockDoorPanel);
-    }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
         securityCenter.switchOn();
-        assertTrue(mockDoorPanel.isCloseBeCalled());
+        verify(mockDoorPanel,times(1)).close();
     }
 }
