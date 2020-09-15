ce29f056f2f9abd7730ffa03dd56864a2633c239
Tingzi123 <1044373556@qq.com>
2020-09-15 11:51:06 +0800

    [chenting]:stub with manual implementation

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -4,16 +4,38 @@ package mock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.stream.Stream;
+
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
+    private static class MockDoorPanel extends DoorPanel{
+        void close() {
+            Stream.of("East Door", "West Door", "North Door", "South Door")
+                    .forEach((door) -> {
+                    /*
+                    第一步：调用第三方服务连接开关门系统接口...
+                    第二步：调用第三方服务检查门的开关状态...
+                    第三步：调用第三方服务检查门开关系统是否正常
+                    第四步：请求关门
+                    第五步：自动关门
+                    第六步：检查门是否成功关闭
+                    第七步：检查门关闭后其他系统是否正常
+                    */
+                        System.out.println(door + "is closed");
+                    });
+        }
+    }
 
     @BeforeEach
     public void setUp() {
+        DoorPanel doorPanel=new DoorPanel();
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        SecurityCenter securityCenter=new SecurityCenter(new DoorPanel());
+        securityCenter.switchOn();
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,34 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    private static  class MockGradeSystem extends GradeSystem{
+        @Override
+        public List<Double> gradesFor(long studentId) {
+            return Arrays.asList(80.0, 90.0, 100.0);
+        }
+    }
+
+    private GradeService gradeService;
+    @BeforeEach
+    public void setup(){
+        MockGradeSystem mockGradeSystem=new MockGradeSystem();
+        gradeService=new GradeService(mockGradeSystem);
+    }
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        double result=gradeService.calculateAverageGrades(1);
+        Assertions.assertEquals(90.0, result);
     }
 }
