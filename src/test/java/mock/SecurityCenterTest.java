62c81de816d5bf8f87e2625d1f751d50016d80a1
Tingzi123 <1044373556@qq.com>
2020-09-15 18:01:51 +0800

    [chenting]:use mock fake door close

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -4,38 +4,38 @@ package mock;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.stream.Stream;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
     private static class MockDoorPanel extends DoorPanel{
+        private  Boolean closeBeCalled=false;
+
+        @Override
         void close() {
-            Stream.of("East Door", "West Door", "North Door", "South Door")
-                    .forEach((door) -> {
-                    /*
-                    第一步：调用第三方服务连接开关门系统接口...
-                    第二步：调用第三方服务检查门的开关状态...
-                    第三步：调用第三方服务检查门开关系统是否正常
-                    第四步：请求关门
-                    第五步：自动关门
-                    第六步：检查门是否成功关闭
-                    第七步：检查门关闭后其他系统是否正常
-                    */
-                        System.out.println(door + "is closed");
-                    });
+            closeBeCalled=true;
         }
+
+        public boolean isCloseBeCalled(){
+            return closeBeCalled;
+        }
+
     }
 
+    private SecurityCenter securityCenter;
+    private MockDoorPanel mockDoorPanel;
     @BeforeEach
     public void setUp() {
-        DoorPanel doorPanel=new DoorPanel();
+        mockDoorPanel=new MockDoorPanel();
+        securityCenter=new SecurityCenter(mockDoorPanel);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
-        SecurityCenter securityCenter=new SecurityCenter(new DoorPanel());
         securityCenter.switchOn();
+        assertTrue(mockDoorPanel.isCloseBeCalled());
     }
 }
