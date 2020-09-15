61dce6555084fe1910dbe26064bc7633cc1cd72a
Tingzi123 <1044373556@qq.com>
2020-09-15 19:39:20 +0800

    [chenting] use mockito fake gradesystem to count average score

--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,34 +1,33 @@
 package stub;
 
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-import java.util.Arrays;
-import java.util.List;
-
+@ExtendWith(MockitoExtension.class)
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
-    private static  class MockGradeSystem extends GradeSystem{
+    /*private static  class MockGradeSystem extends GradeSystem{
         @Override
         public List<Double> gradesFor(long studentId) {
             return Arrays.asList(80.0, 90.0, 100.0);
         }
-    }
+    }*/
+    @Mock
+    private GradeSystem mockGradeSystem;
 
+    @InjectMocks
     private GradeService gradeService;
-    @BeforeEach
-    public void setup(){
-        MockGradeSystem mockGradeSystem=new MockGradeSystem();
-        gradeService=new GradeService(mockGradeSystem);
-    }
 
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        double result=gradeService.calculateAverageGrades(1);
-        Assertions.assertEquals(90.0, result);
+        gradeService.calculateAverageGrades(1);
+        verify(mockGradeSystem,times(1)).gradesFor(1);
     }
 }
